27e4593766e385316d9858618facc6c2fbdc5a21
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2023-12-07 20:42:52 +0100

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from "./menu/menu.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, MenuComponent]
 })
 export class AppComponent {
   title = 'myngapp';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProposComponent } from './propos/propos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' }, // redirect to `home` (bonne pratique)
+    {path:'home',component:HomeComponent},
+    {path:'propos', component:ProposComponent}
+];
